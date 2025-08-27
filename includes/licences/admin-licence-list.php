2fa9d72df320a246e218d5fb1992e08f8cb10b3b
DavyReactiv <davycteam@gmail.com>
2025-08-27 23:36:59 +0200

    Assign DataTable id for club license list

--- includes/licences/admin-licence-list.php
+++ includes/licences/admin-licence-list.php
@@ -229,6 +229,7 @@ $export_nonce = wp_create_nonce('ufsc_export_licences_' . $club_id);
             ob_start();
             $list_table->display();
             $table_html = ob_get_clean();
+            $table_html = str_replace( '<table', '<table id="licenses-table-club"', $table_html );
             $table_html = str_replace( 'wp-list-table widefat fixed striped', 'ufsc-table', $table_html );
             $table_html = preg_replace( '/<tr(?! class=)/', '<tr class="ufsc-row"', $table_html );
             echo $table_html;
