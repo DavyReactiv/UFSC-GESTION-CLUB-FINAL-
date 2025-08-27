02ec483e5148715faa0dacdc78f74c5e96b5b0dc
DavyReactiv <davycteam@gmail.com>
2025-08-28 01:52:00 +0200

    Refactor licence form include and remove duplicate

--- includes/licences/admin-licence-form.php
+++ includes/licences/admin-licence-form.php
@@ -7,7 +7,9 @@ if (!current_user_can('ufsc_manage_own')) {
     wp_die(__('Access denied.', 'plugin-ufsc-gestion-club-13072025'));
 }
 
-require_once UFSC_PLUGIN_PATH . 'includes/licences/class-licence-repository.php';
+if ( ! class_exists( 'UFSC_Licence_Repository' ) ) {
+    require_once UFSC_PLUGIN_PATH . 'includes/repository/class-licence-repository.php';
+}
 require_once UFSC_PLUGIN_PATH . 'includes/licences/validation.php';
 
 $repo       = new UFSC_Licence_Repository();
@@ -87,11 +89,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && check_admin_referer('ufsc_license_a
     }
 }
 
-function ufsc_get_club_name($club_id) {
-    global $wpdb;
-    return $club_id ? $wpdb->get_var($wpdb->prepare("SELECT nom FROM {$wpdb->prefix}ufsc_clubs WHERE id=%d", $club_id)) : '';
-}
-
 $action_url = admin_url('admin.php?page=ufsc_license_add_admin' . ($licence_id ? '&licence_id=' . $licence_id : ''));
 
 ?>
@@ -122,71 +119,3 @@ $action_url = admin_url('admin.php?page=ufsc_license_add_admin' . ($licence_id ?
         </div>
     </form>
 </div>
-
-wp_enqueue_script('jquery-ui-autocomplete');
-wp_localize_script(
-    'jquery-ui-autocomplete',
-    'ufscClubSearch',
-    [
-        'nonce' => wp_create_nonce('ufsc_club_search'),
-    ]
-);
-?>
-<div class="wrap">
-<h1><?php echo $licence_id ? esc_html__('Modifier une licence', 'plugin-ufsc-gestion-club-13072025') : esc_html__('Ajouter une licence', 'plugin-ufsc-gestion-club-13072025'); ?></h1>
-<form method="post" action="<?php echo esc_url($action_url); ?>">
-<?php wp_nonce_field('ufsc_license_admin_action', 'ufsc_license_admin_nonce'); ?>
-    <table class="form-table" role="presentation">
-        <tbody>
-            <tr>
-                <th scope="row"><label for="ufsc-club-search"><?php esc_html_e('Club', 'plugin-ufsc-gestion-club-13072025'); ?></label></th>
-                <td>
-                    <input type="text" id="ufsc-club-search" value="<?php echo esc_attr(ufsc_get_club_name($licence->club_id ?? 0)); ?>" class="regular-text" />
-                    <input type="hidden" name="club_id" id="ufsc-club-id" value="<?php echo esc_attr($licence->club_id ?? 0); ?>" />
-                </td>
-            </tr>
-            <tr>
-                <th scope="row"><label for="nom"><?php esc_html_e('Nom', 'plugin-ufsc-gestion-club-13072025'); ?></label></th>
-                <td><input name="nom" id="nom" type="text" value="<?php echo esc_attr($licence->nom ?? ''); ?>" class="regular-text" required></td>
-            </tr>
-            <tr>
-                <th scope="row"><label for="prenom"><?php esc_html_e('Prénom', 'plugin-ufsc-gestion-club-13072025'); ?></label></th>
-                <td><input name="prenom" id="prenom" type="text" value="<?php echo esc_attr($licence->prenom ?? ''); ?>" class="regular-text" required></td>
-            </tr>
-            <tr>
-                <th scope="row"><label for="email"><?php esc_html_e('Email', 'plugin-ufsc-gestion-club-13072025'); ?></label></th>
-                <td><input name="email" id="email" type="email" value="<?php echo esc_attr($licence->email ?? ''); ?>" class="regular-text" required></td>
-            </tr>
-            <tr>
-                <th scope="row"><label for="date_naissance"><?php esc_html_e('Date de naissance', 'plugin-ufsc-gestion-club-13072025'); ?></label></th>
-                <td><input name="date_naissance" id="date_naissance" type="date" value="<?php echo esc_attr($licence->date_naissance ?? ''); ?>" required></td>
-            </tr>
-            <tr>
-                <th scope="row"><label for="categorie"><?php esc_html_e('Catégorie', 'plugin-ufsc-gestion-club-13072025'); ?></label></th>
-                <td><input name="categorie" id="categorie" type="text" value="<?php echo esc_attr($licence->categorie ?? ''); ?>" class="regular-text" required></td>
-            </tr>
-        </tbody>
-    </table>
-    <?php submit_button($licence_id ? __('Mettre à jour la licence', 'plugin-ufsc-gestion-club-13072025') : __('Ajouter la licence', 'plugin-ufsc-gestion-club-13072025')); ?>
-</form>
-</div>
-
-<script>
-jQuery(function($){
-    $('#ufsc-club-search').autocomplete({
-        source: function(request, response){
-            $.getJSON(ajaxurl, {action: 'ufsc_club_search', term: request.term, nonce: ufscClubSearch.nonce}, function(res){
-                if (res.success) {
-                    response($.map(res.data, function(item){ return { label: item.label, value: item.label, id: item.id }; }));
-                } else {
-                    response([]);
-                }
-            });
-        },
-        select: function(event, ui){
-            $('#ufsc-club-id').val(ui.item.id);
-        }
-    });
-});
-</script>
-
